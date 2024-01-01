fa11c334dc918fd0faa5dc615f9952b1f5e15b4b
Rodrigo <vojta.vesecky@seznam.cz>
2024-01-01 13:31:06 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/net/matez/wildnature/common/objects/features/FeatureRegistry.java
    #	src/main/java/net/matez/wildnature/common/objects/features/ModWorldEvents.java
    #	src/main/java/net/matez/wildnature/common/objects/features/WNCombinedFeature.java
    #	src/main/java/net/matez/wildnature/common/objects/features/WNTreeFeature.java
    #	src/main/java/net/matez/wildnature/setup/WildNature.java
    #	src/main/resources/META-INF/mods.toml

--- src/main/java/net/matez/wildnature/common/registry/biomes/WNRegion.java
+++ src/main/java/net/matez/wildnature/common/registry/biomes/WNRegion.java
@@ -37,8 +37,8 @@ public class WNRegion extends Region {
     {
         super(name, RegionType.OVERWORLD, weight);
     }
-    private final Climate.Parameter FULL_RANGE = Climate.Parameter.span(-1.0F, 1.0F);
-    private final Climate.Parameter[] temperatures = new Climate.Parameter[]{
+    private static final Climate.Parameter FULL_RANGE = Climate.Parameter.span(-1.0F, 1.0F);
+    private static final Climate.Parameter[] temperatures = new Climate.Parameter[]{
             Climate.Parameter.span(-1.0F, -0.45F),
             Climate.Parameter.span(-0.45F, -0.15F),
             Climate.Parameter.span(-0.15F, 0.2F),
@@ -46,7 +46,7 @@ public class WNRegion extends Region {
             Climate.Parameter.span(0.55F, 1.0F)
     };
 
-    private final Climate.Parameter[] humidities = new Climate.Parameter[]{
+    private static final Climate.Parameter[] humidities = new Climate.Parameter[]{
             Climate.Parameter.span(-1.0F, -0.35F),
             Climate.Parameter.span(-0.35F, -0.1F),
             Climate.Parameter.span(-0.1F, 0.1F),
@@ -54,11 +54,11 @@ public class WNRegion extends Region {
             Climate.Parameter.span(0.3F, 1.0F)
     };
 
-    private final Climate.Parameter[] erosions = new Climate.Parameter[]{
+    private static final Climate.Parameter[] erosions = new Climate.Parameter[]{
             Climate.Parameter.span(0F, 1F),
             Climate.Parameter.span(-1F, 0F)
     };
-    private final Climate.Parameter[] weirdness = new Climate.Parameter[]{
+    private static final Climate.Parameter[] weirdness = new Climate.Parameter[]{
         /*
         Climate.Parameter.span(-1.0F, -0.93333334F),    //MID1
         Climate.Parameter.span(-0.93333334F, -0.7666667F),  //HIGH1
@@ -90,23 +90,23 @@ public class WNRegion extends Region {
     protected static final Climate.Parameter COMMON_RARENESS_RANGE     =     Climate.Parameter.span(-1.0F, 0.35F);
     protected static final Climate.Parameter RARE_RARENESS_RANGE     =     Climate.Parameter.span(0.35F, 1.0F);
 
-    protected final Climate.Parameter FROZEN_RANGE     =     this.temperatures[0];
-    protected final Climate.Parameter UNFROZEN_RANGE     =     Climate.Parameter.span(this.temperatures[1], this.temperatures[4]);
-    protected final Climate.Parameter mushroomFieldsContinentalness     =     Climate.Parameter.span(-1.2F, -1.05F);
-    protected final Climate.Parameter deepOceanContinentalness     =     Climate.Parameter.span(-1.05F, -0.455F);
-    protected final Climate.Parameter oceanContinentalness     =     Climate.Parameter.span(-0.455F, -0.19F);
-    protected final Climate.Parameter coastContinentalness     =     Climate.Parameter.span(-0.19F, -0.11F);
-    protected final Climate.Parameter inlandContinentalness     =     Climate.Parameter.span(-0.11F, 0.55F);
-    protected final Climate.Parameter nearInlandContinentalness     =     Climate.Parameter.span(-0.11F, 0.03F);
-    protected final Climate.Parameter midInlandContinentalness     =     Climate.Parameter.span(0.03F, 0.3F);
-    protected final Climate.Parameter farInlandContinentalness     =     Climate.Parameter.span(0.3F, 1.0F);
+    protected static final Climate.Parameter FROZEN_RANGE     =     temperatures[0];
+    protected static final Climate.Parameter UNFROZEN_RANGE     =     Climate.Parameter.span(temperatures[1], temperatures[4]);
+    protected static final Climate.Parameter mushroomFieldsContinentalness     =     Climate.Parameter.span(-1.2F, -1.05F);
+    protected static final Climate.Parameter deepOceanContinentalness     =     Climate.Parameter.span(-1.05F, -0.455F);
+    protected static final Climate.Parameter oceanContinentalness     =     Climate.Parameter.span(-0.455F, -0.19F);
+    protected static final Climate.Parameter coastContinentalness     =     Climate.Parameter.span(-0.19F, -0.11F);
+    protected static final Climate.Parameter inlandContinentalness     =     Climate.Parameter.span(-0.11F, 0.55F);
+    protected static final Climate.Parameter nearInlandContinentalness     =     Climate.Parameter.span(-0.11F, 0.03F);
+    protected static final Climate.Parameter midInlandContinentalness     =     Climate.Parameter.span(0.03F, 0.3F);
+    protected static final Climate.Parameter farInlandContinentalness     =     Climate.Parameter.span(0.3F, 1.0F);
 
-    private final ResourceKey<Biome>[][] OCEANS     =     new ResourceKey[][]{
+    private static final ResourceKey<Biome>[][] OCEANS     =     new ResourceKey[][]{
             {Biomes.DEEP_FROZEN_OCEAN, Biomes.DEEP_COLD_OCEAN, Biomes.DEEP_OCEAN, Biomes.DEEP_LUKEWARM_OCEAN, Biomes.WARM_OCEAN},
             {Biomes.FROZEN_OCEAN,      Biomes.COLD_OCEAN,      Biomes.OCEAN,      Biomes.LUKEWARM_OCEAN,      Biomes.WARM_OCEAN}
     };
 
-    private final ResourceKey<Biome>[][][][] COASTLANDS = new ResourceKey[][][][]{
+    private static final ResourceKey<Biome>[][][][] COASTLANDS = new ResourceKey[][][][]{
 //↓ Temperature \ Humidity →
     //LOWLAND
             {{//PLAINS
@@ -154,7 +154,7 @@ public class WNRegion extends Region {
     };
 
 
-    private final ResourceKey<Biome>[][][][] INLANDS = new ResourceKey[][][][]{
+    private static final ResourceKey<Biome>[][][][] INLANDS = new ResourceKey[][][][]{
 //↓ Temperature \ Humidity →
     //LOWLAND
             {{//PLAINS
@@ -204,82 +204,73 @@ public class WNRegion extends Region {
     @Override
     public void addBiomes(Registry<Biome> registry, Consumer<Pair<Climate.ParameterPoint, ResourceKey<Biome>>> mapper)
     {
-        this.addGrasslands(registry,mapper);
-
+        addGrasslands(registry,mapper);
     }
-    private void addGrasslands(Registry<Biome> biomeRegistry, Consumer<Pair<Climate.ParameterPoint, ResourceKey<Biome>>> mapper)
+    private static void addGrasslands(Registry<Biome> biomeRegistry, Consumer<Pair<Climate.ParameterPoint, ResourceKey<Biome>>> mapper)
     {
         WildNature.getLogger().progress("Adding biomes 3x for some reason");
-        this.addSurfaceBiome(mapper, this.FULL_RANGE, this.FULL_RANGE, Climate.Parameter.span(-0.11F, 1F), this.FULL_RANGE, Climate.Parameter.span(-0.06F, 0.06F), 0.0F, Biomes.RIVER);
-        for (int t = 0; t < this.temperatures.length; ++t)
+        addSurfaceBiome(mapper, FULL_RANGE, FULL_RANGE, Climate.Parameter.span(-0.11F, 1F), FULL_RANGE, Climate.Parameter.span(-0.06F, 0.06F), 0.0F, Biomes.RIVER);
+        for (int t = 0; t < temperatures.length; ++t)
         {
-            Climate.Parameter temperature = this.temperatures[t];
+            Climate.Parameter temperature = temperatures[t];
 
-            for (int h = 0; h < this.humidities.length; ++h)
+            for (int h = 0; h < humidities.length; ++h)
             {
-                Climate.Parameter humidity = this.humidities[h];
+                Climate.Parameter humidity = humidities[h];
 
-                for (int e = 0; e < this.erosions.length; ++e)
+                for (int e = 0; e < erosions.length; ++e)
                 {
-                    Climate.Parameter erosion = this.erosions[e];
+                    Climate.Parameter erosion = erosions[e];
             //Adds flatlands
-                    ResourceKey<Biome> coastal = this.pickCoastal(biomeRegistry, e, 0, t, h);
-                    ResourceKey<Biome> inland = this.pickInland(biomeRegistry, e, 0, t, h);
-                    this.addSurfaceBiome(mapper, temperature, humidity,  Climate.Parameter.span(-0.11F, 0.55F), erosion, weirdness[0], 0.0F, coastal);
-                    this.addSurfaceBiome(mapper, temperature, humidity,  Climate.Parameter.span(0.55F, 1F), erosion, weirdness[0], 0.0F, inland);
+                    ResourceKey<Biome> coastal = pickCoastal(biomeRegistry, e, 0, t, h);
+                    ResourceKey<Biome> inland = pickInland(biomeRegistry, e, 0, t, h);
+                    addSurfaceBiome(mapper, temperature, humidity,  Climate.Parameter.span(-0.11F, 0.55F), erosion, weirdness[0], 0.0F, coastal);
+                    addSurfaceBiome(mapper, temperature, humidity,  Climate.Parameter.span(0.55F, 1F), erosion, weirdness[0], 0.0F, inland);
 
             //Adds slopelands
-                    coastal = this.pickCoastal(biomeRegistry, e, 1, t, h);
-                    inland = this.pickInland(biomeRegistry, e, 1, t, h);
+                    coastal = pickCoastal(biomeRegistry, e, 1, t, h);
+                    inland = pickInland(biomeRegistry, e, 1, t, h);
                 //rare/small biomes
-                    this.addSurfaceBiome(mapper, temperature, humidity,  Climate.Parameter.span(-0.11F, 1F), erosion, weirdness[1], 0.0F, coastal);
-                    this.addSurfaceBiome(mapper, temperature, humidity,  Climate.Parameter.span(-0.11F, 1F), erosion, weirdness[2], 0.0F, coastal);
+                    addSurfaceBiome(mapper, temperature, humidity,  Climate.Parameter.span(-0.11F, 1F), erosion, weirdness[1], 0.0F, coastal);
+                    addSurfaceBiome(mapper, temperature, humidity,  Climate.Parameter.span(-0.11F, 1F), erosion, weirdness[2], 0.0F, coastal);
                 //cliff/mountain biomes
-                    this.addSurfaceBiome(mapper, temperature, humidity,  Climate.Parameter.span(-0.11F, 1F), erosion, weirdness[3], 0.0F, inland);
-                    this.addSurfaceBiome(mapper, temperature, humidity,  Climate.Parameter.span(-0.11F, 1F), erosion, weirdness[4], 0.0F, inland);
+                    addSurfaceBiome(mapper, temperature, humidity,  Climate.Parameter.span(-0.11F, 1F), erosion, weirdness[3], 0.0F, inland);
+                    addSurfaceBiome(mapper, temperature, humidity,  Climate.Parameter.span(-0.11F, 1F), erosion, weirdness[4], 0.0F, inland);
 
             //Adds peak/hill biomes
-                    coastal = this.pickCoastal(biomeRegistry, e, 2, t, h);
-                    inland = this.pickInland(biomeRegistry, e, 2, t, h);
-                    this.addSurfaceBiome(mapper, temperature, humidity,  Climate.Parameter.span(-0.11F, 0.55F), erosion, weirdness[5], 0.0F, coastal);
-                    this.addSurfaceBiome(mapper, temperature, humidity,  Climate.Parameter.span(-0.11F, 0.55F), erosion, weirdness[6], 0.0F, coastal);
+                    coastal = pickCoastal(biomeRegistry, e, 2, t, h);
+                    inland = pickInland(biomeRegistry, e, 2, t, h);
+                    addSurfaceBiome(mapper, temperature, humidity,  Climate.Parameter.span(-0.11F, 0.55F), erosion, weirdness[5], 0.0F, coastal);
+                    addSurfaceBiome(mapper, temperature, humidity,  Climate.Parameter.span(-0.11F, 0.55F), erosion, weirdness[6], 0.0F, coastal);
 
-                    this.addSurfaceBiome(mapper, temperature, humidity,  Climate.Parameter.span(0.55F, 1F), erosion, weirdness[5], 0.0F, inland);
-                    this.addSurfaceBiome(mapper, temperature, humidity,  Climate.Parameter.span(0.55F, 1F), erosion, weirdness[6], 0.0F, inland);
+                    addSurfaceBiome(mapper, temperature, humidity,  Climate.Parameter.span(0.55F, 1F), erosion, weirdness[5], 0.0F, inland);
+                    addSurfaceBiome(mapper, temperature, humidity,  Climate.Parameter.span(0.55F, 1F), erosion, weirdness[6], 0.0F, inland);
                 }
 
 
             }
-            this.addSurfaceBiome(mapper, temperature, this.FULL_RANGE, this.deepOceanContinentalness, this.FULL_RANGE, this.FULL_RANGE, 0.0F, this.OCEANS[0][t]);
-            this.addSurfaceBiome(mapper, temperature, this.FULL_RANGE, this.oceanContinentalness, this.FULL_RANGE, this.FULL_RANGE, 0.0F, this.OCEANS[1][t]);
-            this.addSurfaceBiome(mapper, temperature, this.FULL_RANGE, this.coastContinentalness, this.FULL_RANGE, Climate.Parameter.span(-0.055F, 0.055F), 0.0F, Biomes.BEACH);
+            addSurfaceBiome(mapper, temperature, FULL_RANGE, deepOceanContinentalness, FULL_RANGE, FULL_RANGE, 0.0F, OCEANS[0][t]);
+            addSurfaceBiome(mapper, temperature, FULL_RANGE, oceanContinentalness, FULL_RANGE, FULL_RANGE, 0.0F, OCEANS[1][t]);
+            addSurfaceBiome(mapper, temperature, FULL_RANGE, coastContinentalness, FULL_RANGE, Climate.Parameter.span(-0.055F, 0.055F), 0.0F, Biomes.BEACH);
         }
         WildNature.getLogger().success("Biomes added");
     }
 
 
-    private ResourceKey<Biome> pickCoastal(Registry<Biome> biomeRegistry, int erosionIndex, int slopeIndex, int temperatureIndex, int humidityIndex)
-    {return biomeOrFallback(biomeRegistry, this.COASTLANDS[erosionIndex][slopeIndex][temperatureIndex][humidityIndex],Biomes.SUNFLOWER_PLAINS);}
-    private ResourceKey<Biome> pickInland(Registry<Biome> biomeRegistry, int erosionIndex, int slopeIndex, int temperatureIndex, int humidityIndex)
-    {return biomeOrFallback(biomeRegistry, this.INLANDS[erosionIndex][slopeIndex][temperatureIndex][humidityIndex],Biomes.FLOWER_FOREST);}
+    private static ResourceKey<Biome> pickCoastal(Registry<Biome> biomeRegistry, int erosionIndex, int slopeIndex, int temperatureIndex, int humidityIndex)
+    {return biomeOrFallback(biomeRegistry, COASTLANDS[erosionIndex][slopeIndex][temperatureIndex][humidityIndex],Biomes.SUNFLOWER_PLAINS);}
+    private static ResourceKey<Biome> pickInland(Registry<Biome> biomeRegistry, int erosionIndex, int slopeIndex, int temperatureIndex, int humidityIndex)
+    {return biomeOrFallback(biomeRegistry, INLANDS[erosionIndex][slopeIndex][temperatureIndex][humidityIndex],Biomes.FLOWER_FOREST);}
 
 
-    private void addSurfaceBiome(Consumer<Pair<Climate.ParameterPoint, ResourceKey<Biome>>> mapper, Climate.Parameter temperature, Climate.Parameter humidity, Climate.Parameter continentalness, Climate.Parameter erosion, Climate.Parameter weirdness, float offset, ResourceKey<Biome> biome)
+    private static void addSurfaceBiome(Consumer<Pair<Climate.ParameterPoint, ResourceKey<Biome>>> mapper, Climate.Parameter temperature, Climate.Parameter humidity, Climate.Parameter continentalness, Climate.Parameter erosion, Climate.Parameter weirdness, float offset, ResourceKey<Biome> biome)
     {
         mapper.accept(Pair.of(Climate.parameters(temperature, humidity, continentalness, erosion, Climate.Parameter.point(0.0F), weirdness, offset), biome));
         mapper.accept(Pair.of(Climate.parameters(temperature, humidity, continentalness, erosion, Climate.Parameter.point(1.0F), weirdness, offset), biome));
     }
 
-    private static int index = 0;
-    private static final ResourceKey<Biome>[] list = WNBiomes.getAllBiomes();
-
     private static ResourceKey<Biome> biomeOrFallback(Registry<Biome> biomeRegistry, ResourceKey<Biome>... biomes)
     {
-        /*index ++;
-        if (index >= list.length) {
-            index = 0;
-        }
-        return list[index];*/
         for (ResourceKey<Biome> key : biomes)
         {
 
